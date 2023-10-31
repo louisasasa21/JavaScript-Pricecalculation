ebfb3b6575e9d7ee6689c6aa167d5589168ea5bd
LOUIS <137705341+louisasasa21@users.noreply.github.com>
2023-10-31 12:10:34 +0700

    Add files via upload

---
+++ array/array3.js
@@ -0,0 +1,22 @@
+// let ages =[30, 40, 50, 60, 80]
+// if(ages.includes(40)) {
+//     console.log('you have 40 in list')
+// }
+
+let ages =[35, 14, 20]
+
+console.log('new age', ages)
+
+ages.sort()
+console.log('new age', ages) // คำสั่งเรียงใหม่ 
+
+
+let names_list = [ 'Louis', 'Nurat', 'Do']
+names_list.push('Bee')
+
+
+console.log('name_list', names_list.length)
+
+for (let index = 0 ; index < names_list.length ; index++) {
+    console.log('name_list', names_list[index]) 
+}
